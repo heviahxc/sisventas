eda199e8b49417a7be17e107b93dda988f20cbb9
heviamc <heviamc@gmail.com>
2020-07-21 19:39:54 -0400

    Initial commit

---
+++ database/seeds/CategoriaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categorias')->insert([
+            'nombre_categoria' => 'Maquinarias',
+            
+        ]);
+        DB::table('categorias')->insert([
+            'nombre_categoria' => 'Herramientas',
+        
+        ]);
+    }
+}
---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,7 @@
+@foreach ($categorias as $categoria)
+    => <b>{{$categoria->nombre_categoria}}</b> <br>
+    <br>
+    <br>
+@endforeach
+
+wena qlo
