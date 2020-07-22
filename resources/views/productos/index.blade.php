9df7c7c1c6a92cd3d331181b16cc45fa352383d0
heviamc <heviamc@gmail.com>
2020-07-22 00:49:26 -0400

    producto

---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,8 @@
+@foreach ($productos as $producto)
+    => <b>{{$producto->nombre}}</b> <br>
+    <br>
+    <br>
+@endforeach
+
+wena maricones qlos
+mdskjhdfklñdskalasdk
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','CategoriaController@index');
+Route::get('/','ProductoController@index');
 
 Route::resource('categorias','CategoriaController');
 
