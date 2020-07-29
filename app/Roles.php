fb0e756c913e053182cedf4d4c6ffd4fd6927a6d
heviahxc <51098811+heviahxc@users.noreply.github.com>
2020-07-29 12:47:32 -0400

    profreso autentificacion

--- app/Roles.php
+++ app/Roles.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
 class Roles extends Model
 {
     protected $table = 'roles';
+
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
 }
